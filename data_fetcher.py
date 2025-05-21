1b0b0800f0ab50d6bbf1405b02ceb57b3adfc7db
Lars Willuweit <larswilluweit@mac.localdomain>
2025-05-21 11:52:27 +0200

    dotenv implemented

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,9 +1,14 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-STR_API_KEY = "3gu5g01FCS8+wfLWZCnvWw==qfY8O3OHyyn8Vi4T"
+load_dotenv()
+
 STR_API_URL = "https://api.api-ninjas.com/v1/animals"
+STR_API_KEY = os.getenv('API_KEY')
 
 def fetch_animals(str_search):
+
     """
       Fetches the animals data for the animal 'animal_name'.
       Returns: a list of animals, each animal is a dictionary:
@@ -20,6 +25,7 @@ def fetch_animals(str_search):
         }
     },
     """
+
     dict_params = {'name': str_search}
     dict_headers = {'X-Api-Key': STR_API_KEY}
     response = requests.get(STR_API_URL, params=dict_params, headers=dict_headers)
